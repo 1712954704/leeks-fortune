4d567045d9385103288f2300b451351e13dc09eb
1712954704 <z1712954704@gmail.com>
2022-01-03 23:41:10 +0800

    规模倒序

--- app/Http/Controllers/Api/LeeksController.php
+++ app/Http/Controllers/Api/LeeksController.php
@@ -23,7 +23,7 @@ class LeeksController extends BaseController
         $total = $fund->count();
         $list = [];
         if ($total > 0){
-            $sql = "select a.date,a.code,b.name,b.manager,b.fundScale,sum(a.worth) total_worth from leeks_fund_worth_detail a inner join leeks_fund b on a.code = b.code where a.date between :start_time and :end_time group by a.code order by total_worth $order limit :limit,:pos";
+            $sql = "select a.date,a.code,b.name,b.manager,b.fundScale,sum(a.worth) total_worth from leeks_fund_worth_detail a inner join leeks_fund b on a.code = b.code where a.date between :start_time and :end_time group by a.code order by total_worth $order,b.fundScale desc limit :limit,:pos";
             $list = DB::select($sql, [':start_time' => $start_time,':end_time'=>$end_time,':limit'=>$limit,':pos'=>$pos]);
             $list = array_map('get_object_vars', $list);
         }
