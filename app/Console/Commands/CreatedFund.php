2dd910d7950d90cdf4a2d6c91a311db3c3a75ce2
jack <jack.xu@yunio.com>
2022-01-04 10:40:00 +0800

    写入买入费率

--- app/Console/Commands/CreatedFund.php
+++ app/Console/Commands/CreatedFund.php
@@ -190,7 +190,7 @@ class CreatedFund extends Command
             }else{
                 $fundScale = $item['fundScale'];
             }
-            DB::update('update leeks_fund set manager = :manager, fundScale = :fundScale where code = :code', [':manager'=>$item['manager'],':fundScale'=>$fundScale,':code' => $item['code']]);
+            DB::update('update leeks_fund set manager = :manager, fundScale = :fundScale,buyRate = :buyRate where code = :code', [':manager'=>$item['manager'],':fundScale'=>$fundScale,':buyRate'=>$item['buyRate'],':code' => $item['code']]);
             // 历史净值信息
             if (isset($item['netWorthData']) && is_array($item['netWorthData'])){
                 foreach ($item['netWorthData'] as $info){
